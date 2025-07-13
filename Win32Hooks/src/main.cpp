5b3d71b5d7fc2f3aef5c069bdd02b82f2c762956
Lucas Cota <lucasbrunocota@live.com>
2025-07-13 19:11:54 -0300

    refactor: optimization and good practice

--- Win32Hooks/src/main.cpp
+++ Win32Hooks/src/main.cpp
@@ -21,6 +21,8 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hModule);
+
         Init();
         break;
     case DLL_THREAD_ATTACH:
